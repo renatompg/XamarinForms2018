13f7315fb936c9955fb56c89d988e381fdc2e130
Renato Marques Pires Guedes <renatompg@gmail.com>
2019-12-21 22:13:49 -0300

    app 01

---
+++ Secao05/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using App01_ConsultarCEP.Servico;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App01_ConsultarCEP
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BOTAO.Clicked += BOTAO_Clicked;
+        }
+
+        private void BOTAO_Clicked(object sender, EventArgs e)
+        {
+            if (!IsValidCep(CEP.Text))
+            {
+                DisplayAlert("ERRO", "Cep inválido.", "OK");
+                return;
+            }
+            var endereco = ViaCepServico.BuscarEnderecoViaCEP(CEP.Text);
+
+            RESULTADO.Text = JsonConvert.SerializeObject(endereco);
+
+        }
+        private bool IsValidCep(string cep)
+        {
+            bool valido = true;
+
+            if (string.IsNullOrWhiteSpace(cep))
+                valido = false;
+
+            if (cep.Length != 8)
+                valido = false;
+
+            if (!int.TryParse(cep, out var num))
+                valido = false;
+
+            return valido;
+        }
+
+    }
+}
---
+++ Secao05/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/Servico/ViaCepServico.cs
@@ -0,0 +1,21 @@
+﻿using App01_ConsultarCEP.Servico.Modelo;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace App01_ConsultarCEP.Servico
+{
+    public class ViaCepServico
+    {
+        private static string EnderecoURL = "https://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            WebClient wc = new WebClient();
+            var content = wc.DownloadString(string.Format(EnderecoURL, cep));
+            return JsonConvert.DeserializeObject<Endereco>(content);
+        }
+    }
+}
